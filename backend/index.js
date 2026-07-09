ef182403ca815eb1461516bdaa99c898a00e3ab7
MohammedBasha <me@mohammedbasha.com>
2026-07-09 16:45:26 +0300

    using the express middleware

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,36 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import mergedResolvers from './resolvers/index.js';
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@as-integrations/express5";
+
+import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
+
+const app = express();
+
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
     typeDefs: mergedTypeDefs,
     resolvers: mergedResolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server);
+await server.start();
 
-console.log(`🚀  Server ready at: ${url}`);
+app.use(
+    "/",
+    cors(),
+    express.json(),
+    // expressMiddleware accepts the same arguments:
+    // an Apollo Server instance and optional configuration options
+    expressMiddleware(server, {
+        context: async ({ req }) => ({ req }),
+    }),
+);
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/`);
